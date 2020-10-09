9416cd6386d83b1cd31a2887611ecceda0df99d5
pavolhodas <pavolhodas4@gmail.com>
2020-10-09 15:04:53 +0200

    Add array to five numbers and read second

--- Open-Lab-02.01/Exercise.cs
+++ Open-Lab-02.01/Exercise.cs
@@ -4,14 +4,23 @@ namespace Open_Lab_02._01
 {
     public class Exercise
     {
+
         public double[] Create5Numbers()
         {
-            throw new NotImplementedException();
+            double[] numbers = new double[5];
+            numbers[0] = 4;
+            numbers[1] = 6;
+            numbers[2] = 8;
+            numbers[3] = 10;
+            numbers[4] = 12;
+            Console.WriteLine(numbers);
+            return numbers;
         }
 
         public double GetSecond(double[] numbers)
         {
-            throw new NotImplementedException();
+            
+            return numbers[1];
         }
     }
 }
